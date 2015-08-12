64c0e8f7efa66dbfb30447437a69bcf217159260
Rafael Oliveira <rafaelbco@gmail.com>
2015-08-12 09:21:21 -0300

    Improve setup.py and README

--- setup.py
+++ setup.py
@@ -4,46 +4,46 @@ import os.path
 
 version = '0.0.5dev'
 
+
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-long_description = read('rbco', 'wfdocumentator', 'README.txt') + '\n\n' + read('docs', 'HISTORY.txt')
+long_description = read('README.txt') + '\n\n' + read('docs', 'HISTORY.txt')
 
-setup(name='rbco.wfdocumentator',
-      version=version,
-      description='An add-on for Zope and Plone which aims to provide '
-        'user-friendly automatically generated documentation about workflow '
-        'definitions.',
-      long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='Rafael Oliveira',
-      author_email='rafaelbco@gmail.com',
-      url="http://code.google.com/p/rbco-wfdocumentator/",
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['rbco'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'rbco.commandwrap<=0.0.99',
-          'prdg.util<=0.0.99',
-      ],
-      
-      
-      extras_require = {
+setup(
+    name='rbco.wfdocumentator',
+    version=version,
+    description='An add-on for Zope and Plone which aims to provide '
+    'user-friendly automatically generated documentation about workflow '
+    'definitions.',
+    long_description=long_description,
+    # Get more strings from
+    # http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Framework :: Plone',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='',
+    author='Rafael Oliveira',
+    author_email='rafaelbco@gmail.com',
+    url='https://github.com/rafaelbco/rbco.wfdocumentator',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['rbco'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'rbco.commandwrap<=0.0.99',
+        'prdg.util<=0.0.99',
+    ],
+    extras_require={
         'test': [
         ]
-      },      
-      
-      entry_points="""      
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
+    },
+    entry_points='''
+    [z3c.autoinclude.plugin]
+    target = plone
+    ''',
 )
